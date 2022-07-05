133282f5375e260d4c2e52d1e0ee10899a5547aa
rremix <admin@example.com>
2022-07-05 23:34:23 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO125_IsPalindrome.java
+++ demo-test/src/test/java/com/leetcode/NO125_IsPalindrome.java
@@ -5,17 +5,32 @@ package com.leetcode;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-
-import static com.leetcode.util.LogUtils.info;
+import static com.leetcode.util.LogUtil.info;
 import static java.lang.Character.isLetterOrDigit;
 import static java.lang.Character.toLowerCase;
 
+/**
+ * 回文字符串
+ * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ *
+ * 示例 1:
+ *
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ * 解释："amanaplanacanalpanama" 是回文串
+ * 示例 2:
+ *
+ * 输入: "race a car"
+ * 输出: false
+ * 解释："raceacar" 不是回文串
+ */
 @Slf4j
 public class NO125_IsPalindrome {
 
     @Test
     public void test() {
-        info(log, isPalindrome("A man, a plan, a canal：Panama"));
+        info(isPalindrome("A man, a plan, a canal：Panama"));
     }
 
     public boolean isPalindrome(String s) {
