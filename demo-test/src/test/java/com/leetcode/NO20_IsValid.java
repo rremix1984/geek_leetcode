67172df2ceb84f06c266622ab5618bdfd5e762df
rremix <admin@example.com>
2022-07-11 22:45:20 +0800

    test

---
+++ demo-test/src/test/java/com/leetcode/NO20_IsValid.java
@@ -0,0 +1,79 @@
+/**
+ * copyright 2022/1/19
+ */
+package com.leetcode;
+
+import org.junit.Test;
+
+import java.util.Stack;
+import static com.leetcode.util.LogUtil.info;
+
+/**
+ * 20. 有效的括号
+ *  
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "()"
+ * 输出：true
+ * 示例2：
+ *
+ * 输入：s = "()[]{}"
+ * 输出：true
+ *
+ */
+public class NO20_IsValid {
+
+    @Test
+    public void test(){
+        info(isValid("({ } )"));
+    }
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        return stack.empty();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+public boolean isValid(String s) {
+    Stack<Character> stack = new Stack<>();
+    char[] chars = s.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+        Character c = chars[i];
+        if (c == '(' || c == '[' || c == '{') {
+            stack.push(c);
+        } else {
+            if (c == ' ') continue;
+            if (stack.empty()) return false;
+            if (stack.peek() != '(' && c == ')') return false;
+            if (stack.peek() != '[' && c == ']') return false;
+            if (stack.peek() != '{' && c == '}') return false;
+            stack.pop();
+        }
+    }
+    return stack.empty();
+}
+*/
