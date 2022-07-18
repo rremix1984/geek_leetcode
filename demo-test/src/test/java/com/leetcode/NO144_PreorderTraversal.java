4547d8f6d4a0cb8c0d7f754597e32915780892e7
rremix <admin@example.com>
2022-07-18 23:36:12 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO144_PreorderTraversal.java
+++ demo-test/src/test/java/com/leetcode/NO144_PreorderTraversal.java
@@ -29,6 +29,18 @@ public class NO144_PreorderTraversal {
 
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            if (node.right != null)
+                stack.push(node.right);
+            if (node.left != null)
+                stack.push(node.left);
+        }
         return res;
     }
 }
@@ -49,17 +61,14 @@ public List<Integer> preorderTraversal(TreeNode root) {
     if (root == null)
         return res;
     Stack<TreeNode> stack = new Stack<>();
-    TreeNode cur = root;
-    stack.push(cur);
+    stack.push(root);
     while (!stack.isEmpty()) {
         TreeNode node = stack.pop();
         res.add(node.val);
-        if (node.right != null) {
+        if (node.right != null)
             stack.push(node.right);
-        }
-        if (node.left != null) {
+        if (node.left != null)
             stack.push(node.left);
-        }
     }
     return res;
 }
