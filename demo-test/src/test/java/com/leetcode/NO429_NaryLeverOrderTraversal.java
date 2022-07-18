7e840457d9d42a66c0538a0e1e7d0524aa989e14
rremix <admin@example.com>
2022-07-19 07:43:43 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO429_NaryLeverOrderTraversal.java
+++ demo-test/src/test/java/com/leetcode/NO429_NaryLeverOrderTraversal.java
@@ -31,20 +31,6 @@ public class NO429_NaryLeverOrderTraversal {
 
     public List<List<Integer>> levelOrder(Node root) {
         List<List<Integer>> ans = new ArrayList<>();
-        Deque<Node> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            List<Integer> level = new ArrayList<>();
-            int cnt = queue.size();
-            for (int i = 0; i < cnt; i++) {
-                Node poll = queue.poll();
-                level.add(poll.val);
-                for (Node child : poll.children) {
-                    queue.offer(child);
-                }
-            }
-            ans.add(level);
-        }
         return ans;
     }
 
