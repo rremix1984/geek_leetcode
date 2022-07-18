0c801aec479e47082cefba68800a03a1f1df6b03
rremix <admin@example.com>
2022-07-19 01:30:46 +0800

    test

---
+++ demo-test/src/test/java/com/leetcode/NO429_NaryLeverOrderTraversal.java
@@ -0,0 +1,38 @@
+/**
+ * copyright 2022/1/19
+ */
+package com.leetcode;
+
+import com.leetcode.util.Node;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.leetcode.util.LogUtil.info;
+
+/**
+    429. N 叉树的层序遍历
+    给定一个 N 叉树，返回其节点值的层序遍历。（即从左到右，逐层遍历）。
+    树的序列化输入是用层序遍历，每组子节点都由 null 值分隔（参见示例）。
+
+    示例 1：
+        输入：root = [1,null,3,2,4,null,5,6]
+        输出：[[1],[3,2,4],[5,6]]
+*/
+public class NO429_NaryLeverOrderTraversal {
+
+    @Test
+    public void test() {
+        info(levelOrder(new Node(1,
+                new Node(3,
+                        new Node(5), new Node(6)), new Node(2), new Node(4))));
+    }
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        return res;
+    }
+
+}
