bfd1350572a05a45c25fb84b2e9c903168e72911
rremix <admin@example.com>
2022-07-21 08:05:08 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO46_Permutations.java
+++ demo-test/src/test/java/com/leetcode/NO46_Permutations.java
@@ -5,21 +5,22 @@ package com.leetcode;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static com.leetcode.util.LogUtil.info;
 
 /**
    （中等）
     46. 全排列
-        给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+        给定一个不含重复数字的数组 nums，
+        返回其所有可能的全排列 。你可以按任意顺序返回答案。
     示例 1：
         输入：nums = [1,2,3]
-        输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+        输出：[[1, 2, 3], [1, 3, 2], [2, 1, 3],
+              [2, 3, 1], [3, 1, 2], [3, 2, 1]]
     示例 2：
-        输入：nums = [0,1]
-        输出：[[0,1],[1,0]]
+        输入：nums = [0, 1]
+        输出：[[0, 1], [1, 0]]
     示例 3：
         输入：nums = [1]
         输出：[[1]]
@@ -31,9 +32,27 @@ public class NO46_Permutations {
         info(permute(new int[]{1, 2, 3}));
     }
 
+    List<List<Integer>> res = new LinkedList<>();
+
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        Deque<Integer> list = new LinkedList<>();
+        backtrack(nums, list);
         return res;
+    }
+
+    void backtrack(int[] nums, Deque<Integer> list) {
+        if (list.size() == nums.length) {
+            res.add(new LinkedList(list));
+            return;
+        }
+
+        for (int num : nums) {
+            if (list.contains(num))
+                continue;
+            list.add(num);
+            backtrack(nums, list);
+            list.removeLast();
+        }
     }
 
 }
