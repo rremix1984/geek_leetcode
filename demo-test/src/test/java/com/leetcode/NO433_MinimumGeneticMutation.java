e89bcaa44a4037cb983377b8ed61dcb69237ec25
rremix <admin@example.com>
2022-07-23 11:16:51 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO433_MinimumGeneticMutation.java
+++ demo-test/src/test/java/com/leetcode/NO433_MinimumGeneticMutation.java
@@ -37,36 +37,48 @@ public class NO433_MinimumGeneticMutation {
 
     @Test
     public void test() {
-        // 3
-        info(minMutation("AAAAACCC", "AACCCCCC",
-            new String[]{"AAAACCCC", "AAACCCCC", "AACCCCCC"}));
+        // 需1步
+//        info(minMutation("AACCGGTT", "AACCGGTA",
+//            new String[]{"AACCGGTA"}));// 1
+        // 需2步
+        info(minMutation("AACCGGTT", "AAACGGTA",
+            new String[]{"AACCGGTA", "AACCGCTA", "AAACGGTA"})); // 2
+        // 需3步
+//        info(minMutation("AAAAACCC", "AACCCCCC",
+//            new String[]{"AAAACCCC", "AAACCCCC", "AACCCCCC"}));// 3
     }
 
     int ans = Integer.MAX_VALUE;
+    boolean[] used;
     public int minMutation(String start, String end, String[] bank) {
-        call(start, end, bank, new boolean[bank.length], 0);
+        used = new boolean[bank.length];
+
+        call(start, end, bank, 0);
+
         if (ans == Integer.MAX_VALUE)
             return -1;
         return ans;
     }
 
-    public void call(String start, String end, String[] bank, boolean[] used, int t) {
-        if (t >= ans)
+    public void call(String start, String end, String[] bank, int level) {
+        if (level >= ans)
             return;
         if (start.equals(end)) {
-            ans = t;
+            ans = level;
         } else {
             int diff = 0;
             for (int i = 0; i < bank.length; i++) {
                 if (used[i])
                     continue;
                 for (int j = 0; j < start.length(); j++) {
+                    // 找到任何一处不同的地方 diff++
+                    // diff就是需要的步数
                     if (start.charAt(j) != bank[i].charAt(j))
                         diff++;
                 }
                 if (diff == 1) {
                     used[i] = true;
-                    call(bank[i], end, bank, used, t + 1);
+                    call(bank[i], end, bank, level + 1);
                     used[i] = false;
                 }
                 diff = 0;
