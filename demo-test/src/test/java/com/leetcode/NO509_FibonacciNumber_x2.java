4fab2e09e43c1e5c183fead10ede39f21e82883a
rremix <admin@example.com>
2022-07-24 22:27:03 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO509_FibonacciNumber_x2.java
+++ demo-test/src/test/java/com/leetcode/NO509_FibonacciNumber_x2.java
@@ -4,10 +4,10 @@
 package com.leetcode;
 
 import org.junit.Test;
-
 import static com.leetcode.util.LogUtil.info;
 
 /**
+    （简单）
     509. 斐波那契数
         斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
         F(0) = 0，F(1) = 1
