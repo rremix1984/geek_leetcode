bf8623734b8fec776e4e336741ec1b7d2e7997b9
rremix <admin@example.com>
2022-07-26 21:20:05 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO55_JumpGame_x3.java
+++ demo-test/src/test/java/com/leetcode/NO55_JumpGame_x3.java
@@ -30,12 +30,6 @@ public class NO55_JumpGame_x3 {
     }
 
     public boolean canJump(int[] nums) {
-        int reach = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i > reach)
-                return false;
-            reach = Math.max(reach, i + nums[i]);
-        }
         return true;
     }
 
