2e0bbd58ef8f7f6ef3d5e5e80f38d7bb17ff2870
rremix <admin@example.com>
2022-08-08 21:06:37 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO212_WordSearchII.java
+++ demo-test/src/test/java/com/leetcode/NO212_WordSearchII.java
@@ -22,20 +22,19 @@ import java.util.*;
         输出：[]
 */
 public class NO212_WordSearchII {
+
     int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public List<String> findWords(char[][] board, String[] words) {
         Trie trie = new Trie();
-        for (String word : words) {
+        for (String word : words)
             trie.insert(word);
-        }
 
         Set<String> ans = new HashSet<>();
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[0].length; ++j) {
+        for (int i = 0; i < board.length; ++i)
+            for (int j = 0; j < board[0].length; ++j)
                 dfs(board, trie, i, j, ans);
-            }
-        }
+
         return new ArrayList<>(ans);
     }
 
@@ -50,7 +49,8 @@ public class NO212_WordSearchII {
 
         board[i1][j1] = '#';
         for (int[] dir : dirs) {
-            int i2 = i1 + dir[0], j2 = j1 + dir[1];
+            int i2 = i1 + dir[0];
+            int j2 = j1 + dir[1];
             if (i2 >= 0 && i2 < board.length && j2 >= 0 && j2 < board[0].length)
                 dfs(board, now, i2, j2, ans);
         }
