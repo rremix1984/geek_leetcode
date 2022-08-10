274c8d77566b78a4f955dd157a0fdc8e42555512
rremix <admin@example.com>
2022-08-10 22:37:07 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO191_NumberOf1Bits_x2.java
+++ demo-test/src/test/java/com/leetcode/NO191_NumberOf1Bits_x2.java
@@ -39,11 +39,6 @@ public class NO191_NumberOf1Bits_x2 {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int ret = 0;
-        for (int i = 0; i < 32; i++) {
-            if ((n & (1 << i)) != 0) {
-                ret++;
-            }
-        }
         return ret;
     }
 
