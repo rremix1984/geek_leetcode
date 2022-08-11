c203b32d92f80c2b5e75ac6769f56ec9e8cdd65e
rremix <admin@example.com>
2022-08-11 23:10:03 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO52_NQueensII.java
+++ demo-test/src/test/java/com/leetcode/NO52_NQueensII.java
@@ -4,10 +4,8 @@
 package com.leetcode;
 
 import org.junit.Test;
-
 import java.util.HashSet;
 import java.util.Set;
-
 import static com.leetcode.util.LogUtil.info;
 
 /**
@@ -31,40 +29,42 @@ public class NO52_NQueensII {
     }
 
 
-    public int totalNQueens(int n) {
-        Set<Integer> columns = new HashSet<>();
-        Set<Integer> diagonals1 = new HashSet<>();
-        Set<Integer> diagonals2 = new HashSet<>();
-        return backtrack(n, 0, columns, diagonals1, diagonals2);
+    public int totalNQueens(int qNum) {
+        // diagonal 对角线
+        return call(qNum, 0, new HashSet<>(),
+                               new HashSet<>(),
+                               new HashSet<>());
     }
 
-    public int backtrack(int n, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
-        if (row == n) {
+    public int call(int qNum, int row, Set<Integer> columns, Set<Integer> diagonals1, Set<Integer> diagonals2) {
+        if (row == qNum)
             return 1;
-        } else {
-            int count = 0;
-            for (int i = 0; i < n; i++) {
-                if (columns.contains(i)) {
-                    continue;
-                }
-                int diagonal1 = row - i;
-                if (diagonals1.contains(diagonal1)) {
-                    continue;
-                }
-                int diagonal2 = row + i;
-                if (diagonals2.contains(diagonal2)) {
-                    continue;
-                }
-                columns.add(i);
-                diagonals1.add(diagonal1);
-                diagonals2.add(diagonal2);
-                count += backtrack(n, row + 1, columns, diagonals1, diagonals2);
-                columns.remove(i);
-                diagonals1.remove(diagonal1);
-                diagonals2.remove(diagonal2);
-            }
-            return count;
+
+        int count = 0;
+        for (int i = 0; i < qNum; i++) {
+            if (columns.contains(i))
+                continue;
+
+            int diagonal1 = row - i;
+            if (diagonals1.contains(diagonal1))
+                continue;
+
+            int diagonal2 = row + i;
+            if (diagonals2.contains(diagonal2))
+                continue;
+
+            // 回溯法
+            columns.add(i);
+            diagonals1.add(diagonal1);
+            diagonals2.add(diagonal2);
+
+            count += call(qNum, row + 1, columns, diagonals1, diagonals2);
+
+            columns.remove(i);
+            diagonals1.remove(diagonal1);
+            diagonals2.remove(diagonal2);
         }
+        return count;
     }
 
 }
