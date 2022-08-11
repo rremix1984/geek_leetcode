e58f3e680ae24d77f35038c807d781d85ac6e791
rremix <admin@example.com>
2022-08-12 00:02:49 +0800

    test

--- demo-test/src/test/java/com/leetcode/undo/NO338_CountingBits.java
+++ demo-test/src/test/java/com/leetcode/undo/NO338_CountingBits.java
@@ -52,12 +52,11 @@ public class NO338_CountingBits {
 
     public int[] countBits(int n) {
         int[] bits = new int[n + 1];
-        int highBit = 0;
-        for (int i = 1; i <= n; i++) {
-            if ((i & (i - 1)) == 0)
-                highBit = i;
-            bits[i] = bits[i - highBit] + 1;
-        }
+
+        // dp动态规划
+        for (int i = 1; i <= n; i++)
+            bits[i] = bits[i & (i - 1)] + 1;
+
         return bits;
     }
 }
