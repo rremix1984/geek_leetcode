e5a41e120aaa7d717a5e040fc359cbf05e026341
rremix <admin@example.com>
2022-09-04 16:28:40 +0800

    test

--- demo-test/src/test/java/com/leetcode/normal/NO5_N_LongestPalindromicSubstring.java
+++ demo-test/src/test/java/com/leetcode/normal/NO5_N_LongestPalindromicSubstring.java
@@ -28,18 +28,6 @@ public class NO5_N_LongestPalindromicSubstring {
 
     public String longestPalindrome(String s) {
         String res = "";
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                // 一般状态转移方程
-                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1]);
-
-                //
-                if (j - i + 1 > res.length() && dp[i][j])
-                    res = s.substring(i, j + 1);
-            }
-        }
         return res;
     }
 }
