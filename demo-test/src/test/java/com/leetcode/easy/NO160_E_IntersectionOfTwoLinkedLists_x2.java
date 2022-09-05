2890be7c0d4e2c11bcec9e817d20160b6c1bbc49
rremix <admin@example.com>
2022-09-06 07:49:17 +0800

    test

--- demo-test/src/test/java/com/leetcode/easy/NO160_E_IntersectionOfTwoLinkedLists_x2.java
+++ demo-test/src/test/java/com/leetcode/easy/NO160_E_IntersectionOfTwoLinkedLists_x2.java
@@ -1,14 +1,14 @@
 /**
  * copyright 2022/1/19
  */
-package com.leetcode;
+package com.leetcode.easy;
 
 import com.leetcode.util.ListNode;
 import org.junit.Test;
-
 import static com.leetcode.util.LogUtil.info;
 
 /**
+    (简单)
     160. 相交链表
         给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
         图示两个链表在节点 c1 开始相交：
@@ -26,50 +26,47 @@ import static com.leetcode.util.LogUtil.info;
         输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
         输出：Intersected at '8' -> 4 -> 5
         解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
-        从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,6,1,8,4,5]。
-        在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
-        — 请注意相交节点的值不为 1，因为在链表 A 和链表 B 之中值为 1 的节点 (A 中第二个节点和 B 中第三个节点) 是不同的节点。换句话说，它们在内存中指向两个不同的位置，而链表 A 和链表 B 中值为 8 的节点 (A 中第三个节点，B 中第四个节点) 在内存中指向相同的位置。
+             从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,6,1,8,4,5]。
+             在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+             — 请注意相交节点的值不为 1，因为在链表 A 和链表 B 之中值为 1 的节点 (A 中第二个节点和 B 中第三个节点) 是不同的节点。换句话说，它们在内存中指向两个不同的位置，而链表 A 和链表 B 中值为 8 的节点 (A 中第三个节点，B 中第四个节点) 在内存中指向相同的位置。
     示例 2：
         输入：intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
         输出：Intersected at '2'
         解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
-        从各自的表头开始算起，链表 A 为 [1,9,1,2,4]，链表 B 为 [3,2,4]。
-        在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+             从各自的表头开始算起，链表 A 为 [1,9,1,2,4]，链表 B 为 [3,2,4]。
+             在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
     示例 3：
         输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
         输出：null
         解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。
-        由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
-        这两个链表不相交，因此返回 null 。
+             由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+             这两个链表不相交，因此返回 null 。
 */
-public class NO160_E_IntersectionOfTwoLinkedLists {
+public class NO160_E_IntersectionOfTwoLinkedLists_x2 {
 
     @Test
     public void test() {
-        // 链表的公共部分
         ListNode eight = new ListNode(8, 4, 5);
-        // 列表A
-        ListNode headA = new ListNode(4, 1);
-        headA.next = eight;
-        // 列表B
-        ListNode headB = new ListNode(5, 6, 1);
-        headB.next = eight;
-        info(getIntersectionNode(headA, headB));
+        info(getIntersectionNode(
+                new ListNode(4, 1).next(eight),
+                new ListNode(5, 6, 1).next(eight)));
 
-
-        // 链表的公共部分
         ListNode two = new ListNode(2, 4);
-        // 列表A
-        ListNode headA1 = new ListNode(1, 9, 1);
-        headA1.next = two;
-        // 列表B
-        ListNode headB1 = new ListNode(3);
-        headB1.next = two;
-        info(getIntersectionNode(headA1, headB1));
+        info(getIntersectionNode(
+                new ListNode(1, 9, 1).next(two),
+                new ListNode(3).next(two)));
     }
 
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        return null;
+        if (headA == null || headB == null)
+            return null;
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != p2) {
+            p1 = p1 == null? headB : p1.next;
+            p2 = p2 == null? headA : p2.next;
+        }
+        return p1;
     }
 
 }
