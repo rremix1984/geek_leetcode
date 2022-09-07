f658afeeb3aac3f2f20f93c570b6557c49b9cdce
rremix <admin@example.com>
2022-09-07 08:07:28 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO394_N_DecodeString.java
+++ demo-test/src/test/java/com/leetcode/NO394_N_DecodeString.java
@@ -4,6 +4,8 @@
 package com.leetcode;
 
 import org.junit.Test;
+
+import java.util.Deque;
 import java.util.LinkedList;
 import static com.leetcode.util.LogUtil.info;
 
@@ -41,14 +43,16 @@ public class NO394_N_DecodeString {
         StringBuilder res = new StringBuilder();
         // 数字计数器，用于累计大于9的数字
         int multi = 0;
-        LinkedList<Integer> stack_multi = new LinkedList<>();
-        LinkedList<String> stack_res = new LinkedList<>();
+        // 数字队列，19, 31, 51 ...
+        Deque<Integer> queue_multi = new LinkedList<>();
+        // 括号 [] 队列   [abc] ... [bb] ...  [xyz]
+        Deque<String> queue = new LinkedList<>();
         // 一共4种情况，[、]、1-9的数字、字母
         for(Character c : s.toCharArray()) {
             // 第 1 种场景，遇到左括号 "["
             if(c == '[') {
-                stack_multi.addLast(multi);
-                stack_res.addLast(res.toString());
+                queue_multi.addLast(multi);
+                queue.addLast(res.toString());
                 multi = 0;
                 res.setLength(0);
             // 第 2 种场景，遇到右括号 "]"
@@ -57,10 +61,13 @@ public class NO394_N_DecodeString {
             // 把结果放入 res 中
             } else if(c == ']') {
                 StringBuilder tmp = new StringBuilder();
-                int cur_multi = stack_multi.removeLast();
-                for(int i = 0; i < cur_multi; i++)
+                // 获取前面的计数器值
+                int size = queue_multi.removeLast();
+                // 重复 size 遍 res 值
+                for(int i = 0; i < size; i++)
                     tmp.append(res);
-                res = new StringBuilder(stack_res.removeLast() + tmp);
+                // res 一旦被重复过 res 就不需要了，所以每次都是 new 出来的
+                res = new StringBuilder(queue.removeLast() + tmp);
             // 遇到数字,需要对multi变量做累加
             } else if(c >= '0' && c <= '9') {
                 multi = multi * 10 + Integer.parseInt(c + "");
