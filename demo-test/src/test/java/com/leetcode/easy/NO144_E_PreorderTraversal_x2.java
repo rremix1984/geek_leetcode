52a4e919b94a9a5cef31f8294db1b7e76e13c1a8
rremix <admin@example.com>
2022-09-08 07:31:23 +0800

    test

--- demo-test/src/test/java/com/leetcode/easy/NO144_E_PreorderTraversal_x2.java
+++ demo-test/src/test/java/com/leetcode/easy/NO144_E_PreorderTraversal_x2.java
@@ -7,8 +7,10 @@ import com.leetcode.util.TreeNode;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import static com.leetcode.util.LogUtil.info;
+import static org.junit.Assert.assertEquals;
 
 /**
     （简单）
@@ -25,30 +27,45 @@ import static com.leetcode.util.LogUtil.info;
         输出：[1]
     示例 4：
         输入：root = [1,2]
-        输出：[1,2]
+        输出：[1, 2]
     示例 5：
         输入：root = [1,null,2]
-        输出：[1,2]
+        输出：[1, 2]
 */
 public class NO144_E_PreorderTraversal_x2 {
 
     @Test
     public void test() {
-        info(preorderTraversal(new TreeNode(1,
+        assertEquals(new ArrayList<Integer>(){{add(1);add(2);add(3);}},
+            preorderTraversal(new TreeNode(1,
                             null, new TreeNode(2,
                                         3))));// [1, 2, 3]
-        info(preorderTraversal(
-                new TreeNode()));// []
-        info(preorderTraversal(
-                new TreeNode(1)));// [1]
-        info(preorderTraversal(
-                new TreeNode(1,2, null)));// [1, 2]
-        info(preorderTraversal(
-                new TreeNode(1,null,2)));// [1, 2]
+        assertEquals(new ArrayList<Integer>(){{add(0);}},
+            preorderTraversal(new TreeNode()));// []
+        assertEquals(new ArrayList<Integer>(){{add(1);}},
+            preorderTraversal(new TreeNode(1)));// [1]
+        assertEquals(new ArrayList<Integer>(){{add(1);add(2);}},
+            preorderTraversal(new TreeNode(1,2, null)));// [1, 2]
+        assertEquals(new ArrayList<Integer>(){{add(1);add(2);}},
+            preorderTraversal(new TreeNode(1,null,2)));// [1, 2]
     }
 
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+
+            if (node.right != null)
+                stack.push(node.right);
+
+            if (node.left != null)
+                stack.push(node.left);
+        }
         return res;
     }
 }
