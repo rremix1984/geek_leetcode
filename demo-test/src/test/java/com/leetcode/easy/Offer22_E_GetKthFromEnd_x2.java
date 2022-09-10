53bb24ba9b805fe7a8e4f4a2383b1cdcb9ebdc46
rremix <admin@example.com>
2022-09-10 21:34:01 +0800

    test

--- demo-test/src/test/java/com/leetcode/easy/Offer22_E_GetKthFromEnd_x2.java
+++ demo-test/src/test/java/com/leetcode/easy/Offer22_E_GetKthFromEnd_x2.java
@@ -16,7 +16,7 @@ import static com.leetcode.util.LogUtil.info;
         给定一个链表: 1 -> 2-> 3 -> 4 -> 5, 和 k = 2.
         返回链表 4 -> 5.
 */
-public class Offer22_E_GetKthFromEnd {
+public class Offer22_E_GetKthFromEnd_x2 {
 
     @Test
     public void test() {
