f6e8167f877f5d9268cff8c1e6319c3996b22793
rremix <admin@example.com>
2022-09-13 07:58:45 +0800

    test

--- demo-test/src/test/java/com/leetcode/easy/NO111_E_MinimumDepthOfBinaryTree_x3.java
+++ demo-test/src/test/java/com/leetcode/easy/NO111_E_MinimumDepthOfBinaryTree_x3.java
@@ -10,7 +10,7 @@ import static com.leetcode.util.LogUtil.info;
 import static java.lang.Math.min;
 
 /**
-    （简单）
+    (简单)
     111. 二叉树的最小深度
     给定一个二叉树，找出其最小深度。
     最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
@@ -23,21 +23,19 @@ import static java.lang.Math.min;
         输入：root = [2,null,3,null,4,null,5,null,6]
         输出：5
 */
-public class NO111_E_MinimumDepthOfBinaryTree_x2 {
+public class NO111_E_MinimumDepthOfBinaryTree_x3 {
 
     @Test
     public void test() {
-//        info(minDepth(new TreeNode(
-//                3,
-//        9, new TreeNode(20,
-//                        15, 7))));
+        assert 2 == minDepth(new TreeNode(3,
+                        9, new TreeNode(20,
+                                        15, 7)));// 2
 
-        info(minDepth(new TreeNode(
-            2,
-null, new TreeNode(3,
-        null, new TreeNode(4,
-                null, new TreeNode(5,
-                        null, new TreeNode(6))))))); // 5
+        info( minDepth(new TreeNode(2,
+                            null, new TreeNode(3,
+                                    null, new TreeNode(4,
+                                            null, new TreeNode(5,
+                                                    null, new TreeNode(6))))))); // 5
     }
 
     public int minDepth(TreeNode root) {
