7e0e28536ec0773bf9e0f84dd641a09f58932c63
rremix <admin@example.com>
2022-09-18 17:47:09 +0800

    test

--- demo-test/src/test/java/com/leetcode/interval/Interval_16_04_TicTacToeLcci.java
+++ demo-test/src/test/java/com/leetcode/interval/Interval_16_04_TicTacToeLcci.java
@@ -1,7 +1,7 @@
 /**
  * copyright 2022/1/19
  */
-package com.leetcode;
+package com.leetcode.interval;
 
 import org.junit.Test;
 
@@ -44,37 +44,33 @@ public class Interval_16_04_TicTacToeLcci {
         int left = 0; //左斜线
         int right = 0; //右斜线
         boolean flag = false; //记录有没有空格
-
         for (int i = 0; i < length; i++) {
-
             heng = 0; zong = 0;
-
             for (int j = 0; j < length; j++) {
-
                 heng = heng +  (int) board[i].charAt(j);
                 zong = zong + (int) board[j].charAt(i);
-
-                if(board[i].charAt(j) == ' ') flag = true;
-
+                if(board[i].charAt(j) == ' ')
+                    flag = true;
             }
-
             //横纵检查
-            if (heng == (int)'X' * length || zong == (int)'X' * length) return "X";
-            if (heng == (int)'O' * length || zong == (int)'O' * length) return "O";
-
+            if (heng == (int)'X' * length || zong == (int)'X' * length)
+                return "X";
+            if (heng == (int)'O' * length || zong == (int)'O' * length)
+                return "O";
             //两条斜线上的相加
             left = left + (int)board[i].charAt(i);
             right = right + (int)board[i].charAt(length - i - 1);
-
         }
 
         //两条斜线检查
-        if (left == (int)'X' * length || right == (int)'X' * length) return "X";
-        if (left == (int)'O' * length || right == (int)'O' * length) return "O";
+        if (left == (int)'X' * length || right == (int)'X' * length)
+            return "X";
+        if (left == (int)'O' * length || right == (int)'O' * length)
+            return "O";
 
-        if (flag) return "Pending";
+        if (flag)
+            return "Pending";
         return "Draw";
-
     }
 }
 
