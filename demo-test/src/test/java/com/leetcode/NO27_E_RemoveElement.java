f4cd315d10ec4facab17dec5b08e18a1438da2b2
rremix <admin@example.com>
2022-09-19 20:13:14 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO27_E_RemoveElement.java
+++ demo-test/src/test/java/com/leetcode/NO27_E_RemoveElement.java
@@ -52,6 +52,36 @@ public class NO27_E_RemoveElement {
                 nums[left] = nums[right];
                 left++;
             }
+
         return left;
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+// 方法1：
+public int removeElement(int[] nums, int val) {
+    int left = 0;
+    for (int right = 0; right < nums.length; right++)
+        if (nums[right] != val) {
+            nums[left] = nums[right];
+            left++;
+        }
+
+    return left;
+}
+*/
