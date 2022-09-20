ce5036c34ef787549cc3e1f5733b1ae61170582d
rremix <admin@example.com>
2022-09-20 22:41:17 +0800

    test

--- demo-test/src/test/java/com/leetcode/NO23_H_MergeKSortedLists.java
+++ demo-test/src/test/java/com/leetcode/NO23_H_MergeKSortedLists.java
@@ -6,16 +6,14 @@ package com.leetcode;
 import com.leetcode.util.ListNode;
 import org.junit.Test;
 
-import static com.leetcode.util.LogUtil.info;
-
 /**
     (困难)
     23. 合并K个升序链表
         给你一个链表数组，每个链表都已经按升序排列。
         请你将所有链表合并到一个升序链表中，返回合并后的链表。
     示例 1：
-        输入：lists = [[1,4,5],[1,3,4],[2,6]]
-        输出：[1,1,2,3,4,4,5,6]
+        输入：lists = [[1,4,5], [1,3,4], [2,6]]
+        输出：[1, 1, 2, 3, 4, 4, 5, 6]
         解释：链表数组如下：
             [
                 1->4->5,
@@ -72,22 +70,86 @@ public class NO23_H_MergeKSortedLists {
     }
 
     public ListNode mergeTwoLists(ListNode a, ListNode b) {
-        if (a == null || b == null) {
+        // 任何一个为空，就返回另一边
+        if (a == null || b == null)
             return a != null ? a : b;
-        }
-        ListNode head = new ListNode(0);
-        ListNode tail = head, aPtr = a, bPtr = b;
-        while (aPtr != null && bPtr != null) {
-            if (aPtr.val < bPtr.val) {
-                tail.next = aPtr;
-                aPtr = aPtr.next;
+
+        // 虚拟头节点
+        ListNode dummy = new ListNode(0);
+        ListNode pre = dummy;
+        ListNode p1 = a;
+        ListNode p2 = b;
+
+        while (p1 != null && p2 != null) {
+            if (p1.val < p2.val) {
+                pre.next = p1;
+                p1 = p1.next;
             } else {
-                tail.next = bPtr;
-                bPtr = bPtr.next;
+                pre.next = p2;
+                p2 = p2.next;
             }
-            tail = tail.next;
+            pre = pre.next;
         }
-        tail.next = (aPtr != null ? aPtr : bPtr);
-        return head.next;
+        pre.next = (p1 != null ? p1 : p2);
+        return dummy.next;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+// 方法1：分治法
+public ListNode mergeKLists(ListNode[] lists) {
+    return merge(lists, 0, lists.length - 1);
+}
+
+public ListNode merge(ListNode[] lists, int l, int r) {
+    if (l == r)
+        return lists[l];
+
+    if (l > r)
+        return null;
+
+    int mid = (l + r) >> 1;
+    return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+}
+
+public ListNode mergeTwoLists(ListNode a, ListNode b) {
+    // 任何一个为空，就返回另一边
+    if (a == null || b == null)
+        return a != null ? a : b;
+
+    // 虚拟头节点
+    ListNode dummy = new ListNode(0);
+    ListNode pre = dummy;
+    ListNode p1 = a;
+    ListNode p2 = b;
+
+    while (p1 != null && p2 != null) {
+        if (p1.val < p2.val) {
+            pre.next = p1;
+            p1 = p1.next;
+        } else {
+            pre.next = p2;
+            p2 = p2.next;
+        }
+        pre = pre.next;
+    }
+    pre.next = (p1 != null ? p1 : p2);
+    return dummy.next;
+}
+*/
