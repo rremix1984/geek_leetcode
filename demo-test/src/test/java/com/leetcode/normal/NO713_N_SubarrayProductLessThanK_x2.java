3fa09407dbd581a37b6cdd9397690846885f5a4b
rremix <admin@example.com>
2022-09-20 23:47:53 +0800

    test

---
+++ demo-test/src/test/java/com/leetcode/normal/NO713_N_SubarrayProductLessThanK_x2.java
@@ -0,0 +1,46 @@
+/**
+ * copyright 2022/1/19
+ */
+package com.leetcode.normal;
+
+import org.junit.Test;
+
+/**
+    (中等)
+    713. 乘积小于 K 的子数组
+        给你一个整数数组 nums 和一个整数 k ，请你返回子数组内所有元素的乘积严格小于 k 的连续子数组的数目。
+    示例 1：
+        输入：nums = [10,5,2,6], k = 100
+        输出：8
+        解释：8 个乘积小于 100 的子数组分别为：[10]、[5]、[2]、[6]、[10,5]、[5,2]、[2,6]、[5,2,6]。
+             需要注意的是 [10,5,2] 并不是乘积小于 100 的子数组。
+    示例 2：
+        输入：nums = [1,2,3], k = 0
+        输出：0
+*/
+public class NO713_N_SubarrayProductLessThanK_x2 {
+
+    @Test
+    public void test() {
+        assert 8 == numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100);
+        assert 0 == numSubarrayProductLessThanK(new int[]{1, 2, 3}, 0);
+    }
+
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int ret = 0;
+        int sum = 1;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            // 累乘结果
+            sum *= nums[j];
+
+            // 回溯，把不可能的元素扣除
+            while (i <= j && sum >= k)
+                sum /= nums[i++];
+
+            // 可能的(j) 减去不可能的(i) 再 +1 就是结果了
+            ret += j - i + 1;
+        }
+        return ret;
+    }
+}
