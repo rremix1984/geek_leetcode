7db3caa862a9b5979b51f899ffef5d329f09744f
rremix <admin@example.com>
2022-09-22 07:51:52 +0800

    test

--- demo-test/src/test/java/com/leetcode/normal/NO445_N_AddTwoNumbersII_x2.java
+++ demo-test/src/test/java/com/leetcode/normal/NO445_N_AddTwoNumbersII_x2.java
@@ -34,29 +34,6 @@ public class NO445_N_AddTwoNumbersII_x2 {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode ans = null;
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
-        while (l1 != null) {
-            stack1.push(l1.val);
-            l1 = l1.next;
-        }
-        while (l2 != null) {
-            stack2.push(l2.val);
-            l2 = l2.next;
-        }
-        int carry = 0;
-        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
-            int a = stack1.isEmpty()? 0 : stack1.pop();
-            int b = stack2.isEmpty()? 0 : stack2.pop();
-
-            int cur = carry + a + b;
-            carry = cur / 10;
-            cur = cur % 10;
-
-            ListNode curnode = new ListNode(cur);
-            curnode.next = ans;
-            ans = curnode;
-        }
         return ans;
     }
 }
