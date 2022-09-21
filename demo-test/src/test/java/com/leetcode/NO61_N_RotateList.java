762a7386d6476f718ba74c9312cb24117a58b5d6
rremix <admin@example.com>
2022-09-21 23:06:15 +0800

    test

---
+++ demo-test/src/test/java/com/leetcode/NO61_N_RotateList.java
@@ -0,0 +1,96 @@
+/**
+ * copyright 2022/1/19
+ */
+package com.leetcode;
+
+import com.leetcode.util.ListNode;
+import org.junit.Test;
+
+/**
+    (中等)
+    61. 旋转链表
+        给你一个链表的头节点 head ，旋转链表，将链表每个节点向右移动 k 个位置。
+    示例 1：
+        输入：head = {1, 2, 3, 4, 5}, k = 2
+        输出：[4, 5, 1, 2, 3]
+    示例 2：
+        输入：head = {0, 1, 2}, k = 4
+        输出：[2, 0, 1]
+*/
+public class NO61_N_RotateList {
+
+    @Test
+    public void test() {
+        assert new ListNode(4, 5, 1, 2, 3).equals(rotateRight(new ListNode(1, 2, 3, 4, 5), 2));
+        assert new ListNode(2, 0, 1).equals(rotateRight(new ListNode(0, 1, 2), 4));
+    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k == 0 || head == null || head.next == null)
+            return head;
+
+        int n = 1;
+        ListNode iter = head;
+        while (iter.next != null) {
+            iter = iter.next;
+            n++;
+        }
+
+        int add = n - k % n;
+        if (add == n) {
+            return head;
+        }
+
+        iter.next = head;
+        while (add-- > 0) {
+            iter = iter.next;
+        }
+
+        ListNode ret = iter.next;
+        iter.next = null;
+        return ret;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+public ListNode rotateRight(ListNode head, int k) {
+    if (k == 0 || head == null || head.next == null)
+        return head;
+
+    int n = 1;
+    ListNode iter = head;
+    while (iter.next != null) {
+        iter = iter.next;
+        n++;
+    }
+
+    int add = n - k % n;
+    if (add == n)
+        return head;
+
+    iter.next = head;
+    while (add-- > 0)
+        iter = iter.next;
+
+    ListNode ret = iter.next;
+    iter.next = null;
+    return ret;
+}
+*/
