66cc3ad2fc78720513a52ceea961eb5c42864e76
rremix <admin@example.com>
2022-09-24 23:05:38 +0800

    test

--- demo-test/src/test/java/com/offer/normal/OfferII_080_N_Combine_x2.java
+++ demo-test/src/test/java/com/offer/normal/OfferII_080_N_Combine_x2.java
@@ -1,7 +1,7 @@
 /**
  * copyright 2022/1/19
  */
-package com.offer;
+package com.offer.normal;
 
 import org.junit.Test;
 import java.util.ArrayList;
@@ -20,12 +20,13 @@ import static com.leetcode.util.MathUtils.getArray;
         输入: n = 1, k = 1
         输出: [[1]]
 */
-public class OfferII_080_N_Combine {
+public class OfferII_080_N_Combine_x2 {
 
     @Test
     public void test() {
         List<List<Integer>> res = combine(4, 2);
-        ArrayList<ArrayList<Integer>> target = getArray(new int[][]{{2, 4}, {3, 4}, {2, 3}, {1, 2}, {1, 3}, {1, 4}});
+        ArrayList<ArrayList<Integer>> target = getArray(
+            new int[][]{{2, 4}, {3, 4}, {2, 3}, {1, 2}, {1, 3}, {1, 4}});
         assert target.containsAll(res);
         assert res.containsAll(target);
 
@@ -37,31 +38,26 @@ public class OfferII_080_N_Combine {
 
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        dfs(ans, new ArrayList<>(),1, n, k);
+        call(ans, new ArrayList<Integer>(), 1, n, k);
         return ans;
     }
 
-    // 从 1 到 n 最多 k 个元素
-    public void dfs(List<List<Integer>> ans, List<Integer> temp, int cur, int n, int k) {
-        // 剪枝：temp 长度加上区间 [cur, n] 的长度小于 k，不可能构造出长度为 k 的 temp
-        if (temp.size() + (n - cur + 1) < k)
+    private void call(List<List<Integer>> ans, ArrayList<Integer> list, int i, int n, int k) {
+        if (list.size() + (n - i + 1) < k)
             return;
 
-        // 记录合法的答案
-        if (temp.size() == k) {
-            ans.add(new ArrayList<>(temp));
+        if (list.size() == k) {
+            ans.add(new ArrayList<>(list));
             return;
         }
 
-        // 考虑选择当前位置
-        temp.add(cur);
+        list.add(i);
 
-        dfs(ans, temp,cur + 1, n, k);
+        call(ans, list, i + 1, n, k);
 
-        temp.remove(temp.size() - 1);
+        list.remove(list.size() - 1);
 
-        // 考虑不选择当前位置
-        dfs(ans, temp,cur + 1, n, k);
+        call(ans, list, i + 1, n, k);
     }
 
 }
