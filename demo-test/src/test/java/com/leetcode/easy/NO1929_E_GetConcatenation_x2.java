f6f121927978e0fd4dc7b8dafb42626378f89bce
rremix <admin@example.com>
2022-10-04 09:24:30 +0800

    test

---
+++ demo-test/src/test/java/com/leetcode/easy/NO1929_E_GetConcatenation_x2.java
@@ -0,0 +1,79 @@
+/**
+ * copyright 2022/1/19
+ */
+package com.leetcode.easy;
+
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+    (简单)
+    1929. 数组串联
+        给你一个长度为 n 的整数数组 nums 。请你构建一个长度为 2n 的答案数组 ans ，数组下标 从 0 开始计数 ，对于所有 0 <= i < n 的 i ，满足下述所有要求：
+        ans[i] == nums[i]
+        ans[i + n] == nums[i]
+        具体而言，ans 由两个 nums 数组 串联 形成。
+        返回数组 ans 。
+    示例 1：
+        输入：nums = [1,2,1]
+        输出：[1,2,1,1,2,1]
+        解释：数组 ans 按下述方式形成：
+        - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+        - ans = [1,2,1,1,2,1]
+    示例 2：
+        输入：nums = [1,3,2,1]
+        输出：[1,3,2,1,1,3,2,1]
+        解释：数组 ans 按下述方式形成：
+        - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+        - ans = [1,3,2,1,1,3,2,1]
+*/
+public class NO1929_E_GetConcatenation_x2 {
+
+    @Test
+    public void test() {
+        assertArrayEquals(new int[]{1,3,2,1,1,3,2,1}, getConcatenation(new int[]{1,3,2,1}));
+    }
+
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        return ans;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+public int[] getConcatenation(int[] nums) {
+    //1.创建长度为2n的数组ans，n为nums数组的长度
+    int[] ans = new int[nums.length * 2];
+    //2.遍历nums数组，为它赋值
+    for (int i = 0; i < nums.length; i++){
+        ans[i] = nums[i];
+        ans[i + nums.length] = nums[i];
+    }
+    return ans;
+}
+
+// 方法2：
+public int[] getConcatenation(int[] nums) {
+    int[] ans = new int[nums.length * 2];
+    for (int i = 0; i < ans.length; i++)
+        ans[i] = nums[i % nums.length];
+    return ans;
+}
+*/
---
+++ demo-test/src/test/java/com/leetcode/easy/NO1941_E_AreOccurrencesEqual_x2.java
@@ -0,0 +1,73 @@
+/**
+ * copyright 2022/1/19
+ */
+package com.leetcode.easy;
+
+import org.junit.Test;
+
+/**
+    (简单)
+    1941. 检查是否所有字符出现次数相同
+        给你一个字符串 s ，如果 s 是一个 好 字符串，请你返回 true ，否则请返回 false 。
+        如果 s 中出现过的 所有 字符的出现次数 相同 ，那么我们称字符串 s 是 好 字符串。
+    示例 1：
+        输入：s = "abacbc"
+        输出：true
+        解释：s 中出现过的字符为 'a'，'b' 和 'c' 。s 中所有字符均出现 2 次。
+    示例 2：
+        输入：s = "aaabb"
+        输出：false
+        解释：s 中出现过的字符为 'a' 和 'b' 。
+             'a' 出现了 3 次，'b' 出现了 2 次，两者出现次数不同。
+*/
+public class NO1941_E_AreOccurrencesEqual_x2 {
+
+    @Test
+    public void test() {
+        assert areOccurrencesEqual("abacbc");
+        assert !areOccurrencesEqual("aaabb");
+    }
+
+    public boolean areOccurrencesEqual(String s) {
+        return false;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+public boolean areOccurrencesEqual(String s) {
+    int[] c1 = new int[26];
+    for (char c : s.toCharArray())
+        c1[c - 'a']++;
+
+    int left = 0;
+    int right = 25;
+    while (left < right)
+        if (c1[left] == 0)
+            left++;
+        else if (c1[right] == 0)
+            right--;
+        else
+        if (c1[left] == c1[right])
+            left++;
+        else
+            return false;
+    return true;
+}
+*/
