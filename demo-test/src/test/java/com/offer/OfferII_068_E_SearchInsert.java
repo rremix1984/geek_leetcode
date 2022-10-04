210012db045857526c572b971b158520ea515301
rremix <admin@example.com>
2022-10-04 17:38:26 +0800

    test

--- demo-test/src/test/java/com/offer/OfferII_068_E_SearchInsert.java
+++ demo-test/src/test/java/com/offer/OfferII_068_E_SearchInsert.java
@@ -40,18 +40,7 @@ public class OfferII_068_E_SearchInsert {
     }
 
     public int searchInsert(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
         int ans = nums.length;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (target <= nums[mid]) {
-                ans = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
         return ans;
     }
 
