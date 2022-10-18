aa53bded49839a9b4cf9cb3dfbd6fc6b3954c98f
rremix <admin@example.com>
2022-10-18 21:39:41 +0800

    test

---
+++ demo-test/src/test/java/com/leetcode/NO2269_E_DivisorSubstrings.java
@@ -0,0 +1,56 @@
+/**
+ * copyright 2022/1/19
+ */
+package com.leetcode;
+
+import org.junit.Test;
+
+/**
+    (简单)
+    2269. 找到一个数字的 K 美丽值
+        一个整数 num 的 k 美丽值定义为 num 中符合以下条件的 子字符串 数目：
+        子字符串长度为 k 。
+        子字符串能整除 num 。
+        给你整数 num 和 k ，请你返回 num 的 k 美丽值。
+    注意：
+        允许有 前缀 0 。
+        0 不能整除任何值。
+        一个 子字符串 是一个字符串里的连续一段字符序列。
+    示例 1：
+        输入：num = 240, k = 2
+        输出：2
+        解释：以下是 num 里长度为 k 的子字符串：
+             - "240" 中的 "24" ：24 能整除 240 。
+             - "240" 中的 "40" ：40 能整除 240 。
+             所以，k 美丽值为 2 。
+    示例 2：
+        输入：num = 430043, k = 2
+        输出：2
+        解释：以下是 num 里长度为 k 的子字符串：
+             - "430043" 中的 "43" ：43 能整除 430043 。
+             - "430043" 中的 "30" ：30 不能整除 430043 。
+             - "430043" 中的 "00" ：0 不能整除 430043 。
+             - "430043" 中的 "04" ：4 不能整除 430043 。
+             - "430043" 中的 "43" ：43 能整除 430043 。
+             所以，k 美丽值为 2 。
+*/
+public class NO2269_E_DivisorSubstrings {
+
+    @Test
+    public void test() {
+        assert 2 == divisorSubstrings(240,2);
+        assert 2 == divisorSubstrings(430043,2);
+    }
+
+    public int divisorSubstrings(int num, int k) {
+        int ans = 0;
+        String s = String.valueOf(num);
+        for (int i = 0; i <= s.length() - k; i++) {
+            int a = Integer.parseInt(s.substring(i, i + k));
+            if (a > 0 && num % a == 0)
+                ans++;
+        }
+        return ans;
+    }
+
+}
---
+++ demo-test/src/test/java/com/leetcode/easy/NO2441_E_FindMaxK_x2.java
@@ -0,0 +1,76 @@
+/**
+ * copyright 2022/1/19
+ */
+package com.leetcode.easy;
+
+import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
+import static java.lang.Math.abs;
+
+/**
+    (简单)
+    2441. 与对应负数同时存在的最大正整数
+        给你一个不包含任何零的整数数组 nums ，找出自身与对应的负数都在数组中存在的最大正整数 k 。
+        返回正整数 k ，如果不存在这样的整数，返回 -1 。
+    示例 1：
+        输入：nums = {-1, 2, -3, 3}
+        输出：3
+        解释：3 是数组中唯一一个满足题目要求的 k 。
+    示例 2：
+        输入：nums = {-1, 10, 6, 7, -7, 1}
+        输出：7
+        解释：数组中存在 1 和 7 对应的负数，7 的值更大。
+    示例 3：
+        输入：nums = {-10, 8, 6, 7, -2, -3}
+        输出：-1
+        解释：不存在满足题目要求的 k ，返回 -1 。
+*/
+public class NO2441_E_FindMaxK_x2 {
+
+    @Test
+    public void test() {
+        assert 3 == findMaxK(new int[]{-1, 2, -3, 3});
+        assert 7 == findMaxK(new int[]{-1, 10, 6, 7, -7, 1});
+        assert -1 == findMaxK(new int[]{-10, 8, 6, 7, -2, -3});
+    }
+
+    public int findMaxK(int[] nums) {
+        int max = -1;
+        return max;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+public int findMaxK(int[] nums) {
+    int ans = -1;
+    // 使用哈希set记录出现过的数字
+    Set<Integer> set = new HashSet<>();
+    for (int num : nums) {
+        int abs = abs(num);
+        // 出现过相反数，且绝对值大于当前最大值
+        if (set.contains(-num) && abs > ans)
+            // 更新最大值
+            ans = abs;
+
+        set.add(num);
+    }
+    return ans;
+}
+*/
