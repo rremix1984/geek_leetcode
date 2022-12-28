1f1303ff64b6e5ba8f2c944efcff0784ad6ce579
wangxiaozhe1984 <wangxiaozhe1984@163.com>
2022-12-28 15:58:05 +0800

    test

---
+++ geek-normal/src/test/java/com/leetcode/normal/NO1750_N_MinimumLength_x2.java
@@ -0,0 +1,61 @@
+package com.leetcode.normal;
+
+import org.junit.Test;
+
+/**
+    (中等)
+    1750. 删除字符串两端相同字符后的最短长度
+        给你一个只包含字符 'a'，'b' 和 'c' 的字符串 s ，你可以执行下面这个操作（5 个步骤）任意次：
+            选择字符串 s 一个 非空 的前缀，这个前缀的所有字符都相同。
+            选择字符串 s 一个 非空 的后缀，这个后缀的所有字符都相同。
+            前缀和后缀在字符串中任意位置都不能有交集。
+            前缀和后缀包含的所有字符都要相同。
+            同时删除前缀和后缀。
+        请你返回对字符串 s 执行上面操作任意次以后（可能 0 次），能得到的 最短长度 。
+    示例 1：
+        输入：s = "ca"
+        输出：2
+        解释：你没法删除任何一个字符，所以字符串长度仍然保持不变。
+    示例 2：
+        输入：s = "cabaabac"
+        输出：0
+        解释：最优操作序列为：
+        - 选择前缀 "c" 和后缀 "c" 并删除它们，得到 s = "abaaba" 。
+        - 选择前缀 "a" 和后缀 "a" 并删除它们，得到 s = "baab" 。
+        - 选择前缀 "b" 和后缀 "b" 并删除它们，得到 s = "aa" 。
+        - 选择前缀 "a" 和后缀 "a" 并删除它们，得到 s = "" 。
+    示例 3：
+        输入：s = "aabccabba"
+        输出：3
+        解释：最优操作序列为：
+        - 选择前缀 "aa" 和后缀 "a" 并删除它们，得到 s = "bccabb" 。
+        - 选择前缀 "b" 和后缀 "bb" 并删除它们，得到 s = "cca" 。
+    提示：
+        1 <= s.length <= 105
+        s 只包含字符 'a'，'b' 和 'c' 。
+*/
+public class NO1750_N_MinimumLength_x2 {
+
+    @Test
+    public void test() {
+        assert 2 == minimumLength("ca");
+        assert 0 == minimumLength("cabaabac");
+        assert 3 == minimumLength("aabccabba");
+    }
+
+    public int minimumLength(String s) {
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            char c = s.charAt(left);
+            while (left <= right && s.charAt(left) == c) {
+                left++;
+            }
+            while (left <= right && s.charAt(right) == c) {
+                right--;
+            }
+        }
+        return right - left + 1;
+    }
+
+}
