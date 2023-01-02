65b5189ff113d82df0d890feab6635c267b9608a
wangxiaozhe1984 <wangxiaozhe1984@163.com>
2023-01-02 21:58:42 +0800

    test

---
+++ curator-api-example/src/test/java/com/itcast/curator/CuratorConnection.java
@@ -0,0 +1,36 @@
+package com.itcast.curator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryOneTime;
+import org.junit.Test;
+
+@Slf4j
+public class CuratorConnection {
+
+    public static final String IPS = "192.168.31.121:2181,192.168.31.122:2181,192.168.31.123:2181";
+    public static final int TIMEOUT = 5000;
+
+    @Test
+    public void test() {
+        // 创建连接对象
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                // ip地址和端口号
+                .connectString(IPS)
+                // 超时时间
+                .sessionTimeoutMs(TIMEOUT)
+                // 重连机制，超时后的重试策略
+                .retryPolicy(new RetryOneTime(3000))
+                // 命名空间，例如：create 就是以create节点为父节点
+                .namespace("create")
+                // 构建连接对象
+                .build();
+        // 打开连接
+        client.start();
+        log.info("client连接打开: {}", client.isStarted());
+        // 关闭连接
+        client.close();
+    }
+
+}
