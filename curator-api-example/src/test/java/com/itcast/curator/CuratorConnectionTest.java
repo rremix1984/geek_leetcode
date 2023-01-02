85b7f331e8ee847a9d697305aac0e5fc3f8468db
wangxiaozhe1984 <wangxiaozhe1984@163.com>
2023-01-02 22:20:31 +0800

    test

--- curator-api-example/src/test/java/com/itcast/curator/CuratorConnectionTest.java
+++ curator-api-example/src/test/java/com/itcast/curator/CuratorConnectionTest.java
@@ -3,17 +3,20 @@ package com.itcast.curator;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.GetChildrenBuilder;
 import org.apache.curator.retry.RetryOneTime;
 import org.junit.Test;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
-public class CuratorConnection {
+public class CuratorConnectionTest {
 
     public static final String IPS = "192.168.31.121:2181,192.168.31.122:2181,192.168.31.123:2181";
     public static final int TIMEOUT = 5000;
 
     @Test
-    public void test() {
+    public void test() throws Exception {
         // 创建连接对象
         CuratorFramework client = CuratorFrameworkFactory.builder()
                 // ip地址和端口号
@@ -26,9 +29,16 @@ public class CuratorConnection {
                 .namespace("create")
                 // 构建连接对象
                 .build();
+
         // 打开连接
         client.start();
         log.info("client连接打开: {}", client.isStarted());
+        log.info(new String(client.getData().forPath("node1")));
+        GetChildrenBuilder gcb = client.getChildren();
+        List<String> list = gcb.forPath("");
+        Collections.sort(list);
+        list.forEach(System.out::println);
+
         // 关闭连接
         client.close();
     }
