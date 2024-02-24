7c3ea01f4f298631a0b991bf4a92af7f9f2cae1a
wangxiaozhe <rremix1984@gmail.com>
2024-02-24 15:07:59 +0800

    test

--- geek-easy/src/test/java/com/leetcode/easy/NO2357_E_MinimumOperations_x3.java
+++ geek-easy/src/test/java/com/leetcode/easy/NO2357_E_MinimumOperations_x3.java
@@ -5,13 +5,15 @@ package com.leetcode.easy;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
-    [ARRAY]
+    [ARRAY] |
     (简单)
     2357. 使数组中所有元素都等于零
-        给你一个非负整数数组 nums 。在一步操作中，你必须：
-        选出一个正整数 x ，x 需要小于或等于 nums 中 最小 的 非零 元素。
-        nums 中的每个正整数都减去 x。
+        给你一个非负整数数组nums。在一步操作中，你必须：
+        选出一个正整数x，x需要小于或等于nums中最小的非零元素。
+        nums中的每个正整数都减去x。
         返回使 nums 中所有元素都等于 0 需要的 最少 操作数。
     示例 1：
         输入：nums = {1, 5, 0, 3, 5}
@@ -25,7 +27,7 @@ import org.junit.Test;
         输出：0
         解释：nums 中的每个元素都已经是 0 ，所以不需要执行任何操作。
 */
-public class NO2357_E_MinimumOperations_x2 {
+public class NO2357_E_MinimumOperations_x3 {
 
     @Test
     public void test() {
@@ -34,6 +36,8 @@ public class NO2357_E_MinimumOperations_x2 {
     }
 
     public int minimumOperations(int[] nums) {
+        // 2024/2/24 NO.3
+        Arrays.sort(nums);
         int res = 0;
         return res;
     }
@@ -55,7 +59,7 @@ public class NO2357_E_MinimumOperations_x2 {
 
 
 
-/**
+/*
 // 方法1：
 public int minimumOperations(int[] nums) {
     Arrays.sort(nums);
--- geek-easy/src/test/java/com/leetcode/easy/NO2395_E_FindSubarrays_x3.java
+++ geek-easy/src/test/java/com/leetcode/easy/NO2395_E_FindSubarrays_x3.java
@@ -4,14 +4,16 @@
 package com.leetcode.easy;
 
 import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
-    [ARRAY]
+    [ARRAY] |
     (简单)
     2395. 和相等的子数组
-        给你一个下标从 0 开始的整数数组 nums ，判断是否存在 两个 长度为 2 的子数组且它们的 和 相等。注意，这两个子数组起始位置的下标必须 不相同 。
-        如果这样的子数组存在，请返回 true，否则返回 false 。
-        子数组 是一个数组中一段连续非空的元素组成的序列。
+    给你一个下标从0开始的整数数组nums，判断是否存在【两个长度为2的】子数组且它们的和相等。
+    （注意：这两个子数组起始位置的下标必须不相同）。如果这样的子数组存在，请返回true，
+    否则返回false。【子数组】是一个数组中一段【连续非空】的元素组成的序列。
     示例 1：
         输入：nums = {4, 2, 4}
         输出：true
@@ -19,23 +21,25 @@ import org.junit.Test;
     示例 2：
         输入：nums = {1, 2, 3, 4, 5}
         输出：false
-        解释：没有长度为 2 的两个子数组和相等。
+        解释：没有长度为2的两个子数组和相等。
     示例 3：
         输入：nums = {0, 0, 0}
         输出：true
         解释：子数组 [nums[0],nums[1]] 和 [nums[1],nums[2]] 的和相等，都为 0 。
         注意即使子数组的元素相同，这两个子数组也视为不相同的子数组，因为它们在原数组中的起始位置不同。
 */
-public class NO2395_E_FindSubarrays_x2 {
+public class NO2395_E_FindSubarrays_x3 {
 
     @Test
     public void test() {
         assert findSubarrays(new int[]{4, 2, 4});
         assert !findSubarrays(new int[]{1, 2, 3, 4, 5});
         assert findSubarrays(new int[]{0, 0, 0});
+        assert findSubarrays(new int[]{1, 3, 2, 2});
     }
 
     public boolean findSubarrays(int[] nums) {
+        // 2024/2/24 NO.3
         return false;
     }
 
@@ -64,7 +68,7 @@ public class NO2395_E_FindSubarrays_x2 {
 
 
 
-/**
+/*
 // 方法1：
 public boolean findSubarrays(int[] nums) {
     Set<Integer> set = new HashSet<>();
