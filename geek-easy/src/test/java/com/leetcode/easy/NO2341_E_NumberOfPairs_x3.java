67305ef9aaf4b5133107c48f21cd96a90557633e
wangxiaozhe <rremix1984@gmail.com>
2024-02-24 15:46:00 +0800

    test

--- geek-easy/src/test/java/com/leetcode/easy/NO2341_E_NumberOfPairs_x3.java
+++ geek-easy/src/test/java/com/leetcode/easy/NO2341_E_NumberOfPairs_x3.java
@@ -7,22 +7,23 @@ import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
 /**
-    [ARRAY]
+    [ARRAY] |
     (简单)
     2341. 数组能形成多少数对
-        给你一个下标从 0 开始的整数数组 nums 。在一步操作中，你可以执行以下步骤：
-        从 nums 选出 两个 相等的 整数
-        从 nums 中移除这两个整数，形成一个 数对
-        请你在 nums 上多次执行此操作直到无法继续执行。
-        返回一个下标从 0 开始、长度为 2 的整数数组 answer 作为答案，
-        其中 answer[0] 是形成的数对数目，answer[1] 是对 nums 尽可能执行上述操作后剩下的整数数目。
+        给你一个数组nums。在一步操作中，你可以执行以下步骤：
+         1. 从nums选出两个相等的整数,
+         2. 从nums中移除这两个整数，形成一个数对
+        请你在nums上多次执行此操作直到无法继续执行。
+        返回一个下标从 0 开始、长度为 2 的整数数组answer作为答案，
+        其中answer[0]是形成的数对数目，answer[1]是对nums尽可能
+        执行上述操作后【剩下】的【整数数目】。
     示例 1：
         输入：nums = {1, 3, 2, 1, 3, 2, 2}
         输出：{3, 1}
         解释：nums[0] 和 nums[3] 形成一个数对，并从 nums 中移除，nums = [3,2,3,2,2] 。
              nums[0] 和 nums[2] 形成一个数对，并从 nums 中移除，nums = [2,2,2] 。
              nums[0] 和 nums[1] 形成一个数对，并从 nums 中移除，nums = [2] 。
-             无法形成更多数对。总共形成 3 个数对，nums 中剩下 1 个数字。
+             无法形成更多数对。总共形成3个数对，nums中剩下1个数字。
     示例 2：
         输入：nums = {1, 1}
         输出：{1, 0}
@@ -36,7 +37,7 @@ import static org.junit.Assert.assertArrayEquals;
         1 <= nums.length <= 100
         0 <= nums[i] <= 100
 */
-public class NO2341_E_NumberOfPairs_x2 {
+public class NO2341_E_NumberOfPairs_x3 {
 
     @Test
     public void test() {
@@ -49,6 +50,7 @@ public class NO2341_E_NumberOfPairs_x2 {
     }
 
     public int[] numberOfPairs(int[] nums) {
+        // 2024/2/24 NO.3
         int pair = 0;
         int rest = 0;
         return new int[]{pair, rest};
@@ -71,7 +73,8 @@ public class NO2341_E_NumberOfPairs_x2 {
 
 
 
-/**
+/*
+// 方法1
 public int[] numberOfPairs(int[] nums) {
     int pair = 0;
     int rest = 0;
@@ -84,6 +87,7 @@ public int[] numberOfPairs(int[] nums) {
         pair += num / 2;
         rest += num % 2;
     }
+
     return new int[]{pair, rest};
 }
 */
--- geek-normal/src/test/java/com/leetcode/normal/NO784_N_LetterCasePermutation_x3.java
+++ geek-normal/src/test/java/com/leetcode/normal/NO784_N_LetterCasePermutation_x3.java
@@ -8,9 +8,10 @@ import java.util.*;
 
 import static com.leetcode.util.LogUtil.info;
 import static com.leetcode.util.MathUtils.getArray;
+import static java.lang.Character.isDigit;
 
 /**
-    [ARRAY]
+    [ARRAY] |
     (中等)
     784. 字母大小写全排列
         给定一个字符串 s ，通过将字符串 s 中的每个字母转变大小写，我们可以获得一个新的字符串。
@@ -25,17 +26,18 @@ import static com.leetcode.util.MathUtils.getArray;
         1 <= s.length <= 12
         s 由小写英文字母、大写英文字母和数字组成
 */
-public class NO784_N_LetterCasePermutation_x2 {
+public class NO784_N_LetterCasePermutation_x3 {
 
     @Test
     public void test() {
-        assert getArray("a1b2", "a1B2", "A1b2", "A1B2").equals(
-                letterCasePermutation("a1b2"));
-        assert getArray("3z4","3Z4").equals(
-                letterCasePermutation("3z4"));
+        assert getArray("a1b2", "a1B2", "A1b2", "A1B2")
+                .containsAll(letterCasePermutation("a1b2"));
+        assert getArray("3z4","3Z4")
+                .containsAll(letterCasePermutation("3z4"));
     }
 
     public List<String> letterCasePermutation(String s) {
+        // 2024/2/24 NO.3
         List<String> ans = new ArrayList<>();
         return ans;
     }
@@ -59,7 +61,7 @@ public class NO784_N_LetterCasePermutation_x2 {
 
 
 
-/**
+/*
 // 方法1：
 public List<String> letterCasePermutation(String s) {
     List<String> ans = new ArrayList<>();
@@ -86,14 +88,16 @@ public List<String> letterCasePermutation(String s) {
 // 方法2：
 public List<String> letterCasePermutation(String s) {
     List<String> ans = new ArrayList<>();
-    dfs(s.toCharArray(),0, ans);
+    dfs(s.toCharArray(), 0, ans);
     return ans;
 }
 
 public void dfs(char[] arr, int pos, List<String> res) {
+    // 1. 先跳过所有的数字
     while (pos < arr.length && isDigit(arr[pos]))
         pos++;
 
+    // 2. 如果已遍历完了，就把arr加到最终结果集上，并返回
     if (pos == arr.length) {
         res.add(new String(arr));
         return;
@@ -103,10 +107,12 @@ public void dfs(char[] arr, int pos, List<String> res) {
     // 'a' ^ 32 == 'A'
     // 'A' ^ 32 == 'a'
     arr[pos] ^= 32;
+    // 3. 先变换大小写，位置往后走一个，然后传入下一层
     dfs(arr, pos + 1, res);
 
     // 再次异或相当于 减法
     arr[pos] ^= 32;
+    // 4. 还原回去，位置往后走一个，再传入下一层
     dfs(arr, pos + 1, res);
 }
 */
