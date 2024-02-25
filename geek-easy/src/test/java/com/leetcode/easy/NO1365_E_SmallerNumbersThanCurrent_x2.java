8986e046686d2f13fac999e32b828c889b980f9a
wangxiaozhe <rremix1984@gmail.com>
2024-02-25 11:22:44 +0800

    test

--- geek-easy/src/test/java/com/leetcode/easy/NO1365_E_SmallerNumbersThanCurrent_x2.java
+++ geek-easy/src/test/java/com/leetcode/easy/NO1365_E_SmallerNumbersThanCurrent_x2.java
@@ -4,24 +4,28 @@
 package com.leetcode.easy;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.assertArrayEquals;
 
 /**
-    [ARRAY]
+    [ARRAY] |
     (简单)
     1365. 有多少小于当前数字的数字
-        给你一个数组 nums，对于其中每个元素 nums[i]，请你统计数组中比它小的所有数字的数目。
-        换而言之，对于每个 nums[i] 你必须计算出有效的 j 的数量，其中 j 满足 j != i 且 nums[j] < nums[i] 。
+        给你一个数组nums，对于其中每个元素nums[i]，请你统计数组中
+        比它小的所有数字的数目。换而言之，对于每个 nums[i] 你必须
+        计算出有效的j的数量，其中j满足j != i且nums[j] < nums[i]。
         以数组形式返回答案。
     示例 1：
         输入：nums = {8, 1, 2, 2, 3}
         输出：{4, 0, 1, 1, 3}
         解释：
-        对于 nums{0}=8 存在四个比它小的数字：（1，2，2 和 3）。
-        对于 nums{1}=1 不存在比它小的数字。
-        对于 nums{2}=2 存在一个比它小的数字：（1）。
-        对于 nums{3}=2 存在一个比它小的数字：（1）。
-        对于 nums{4}=3 存在三个比它小的数字：（1，2 和 2）。
+            对于 nums{0}=8 存在（4）个比它小的数字：（1，2，2 和 3）。
+            对于 nums{1}=1 不存在（0）比它小的数字。
+            对于 nums{2}=2 存在（1）个比它小的数字：（1）。
+            对于 nums{3}=2 存在（1）个比它小的数字：（1）。
+            对于 nums{4}=3 存在（3）个比它小的数字：（1，2 和 2）。
     示例 2：
         输入：nums = {6, 5, 4, 8}
         输出：{2, 1, 0, 3}
@@ -42,6 +46,7 @@ public class NO1365_E_SmallerNumbersThanCurrent_x2 {
     }
 
     public int[] smallerNumbersThanCurrent(int[] nums) {
+        // 2024/2/25 NO.3
         int[] ret = new int[nums.length];
         return ret;
     }
@@ -63,7 +68,8 @@ public class NO1365_E_SmallerNumbersThanCurrent_x2 {
 
 
 
-/**
+/*
+// 方法1：时间复杂度高
 public int[] smallerNumbersThanCurrent(int[] nums) {
     int[] ret = new int[nums.length];
     for (int i = 0; i < nums.length; i++)
