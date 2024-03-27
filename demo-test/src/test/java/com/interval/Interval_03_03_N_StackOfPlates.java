67c3b9bba327ab662b830315e95af7883c1e4419
wangxiaozhe <rremix1984@gmail.com>
2024-03-28 07:32:46 +0800

    test

---
+++ demo-test/src/test/java/com/interval/Interval_03_03_N_StackOfPlates.java
@@ -0,0 +1,90 @@
+/**
+ * copyright @leetcode.cn
+ */
+package com.interval;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+    [LISTNODE]
+    (中等)
+    Interval.03.03 堆盘子
+    堆盘子。设想有一堆盘子，堆太高可能会倒下来。因此，在现实生活中，盘子堆到一定
+    高度时，我们就会另外堆一堆盘子。请实现数据结构SetOfStacks，模拟这种行为。
+    SetOfStacks应该由多个栈组成，并且在前一个栈填满时新建一个栈。此外，
+    SetOfStacks.push()和SetOfStacks.pop()应该与普通栈的操作方法相同
+    （也就是说，pop()返回的值，应该跟只有一个栈时的情况一样）。
+    进阶：实现一个 popAt(int index)方法，根据指定的子栈，执行pop操作。
+    当某个栈为空时，应当删除该栈。当栈中没有元素或不存在该栈时，pop，popAt 应返回-1.
+    示例1:
+        输入：
+            ["StackOfPlates", "push", "push", "popAt", "pop", "pop"]
+            [[1], [1], [2], [1], [], []]
+        输出：
+            [null, null, null, 2, 1, -1]
+    示例2:
+        输入：
+            ["StackOfPlates", "push", "push", "push", "popAt", "popAt", "popAt"]
+            [[2], [1], [2], [3], [0], [0], [0]]
+        输出：
+            [null, null, null, null, 2, 1, 3]
+    Related Topics:栈,设计,链表
+*/
+public class Interval_03_03_N_StackOfPlates {
+
+    @Test
+    public void test() {
+        StackOfPlates sop = new StackOfPlates(1);
+        sop.push(1);
+        sop.push(2);
+        assert  2 == sop.popAt(1);
+        assert  1 == sop.pop();
+        assert -1 == sop.pop();
+    }
+
+}
+
+class StackOfPlates {
+
+    private List<Stack<Integer>> stackList;
+    private int cap;
+
+    public StackOfPlates(int cap) {
+        this.stackList = new ArrayList<>();
+        this.cap = cap;
+    }
+
+    public void push(int val) {
+        if (this.cap == 0)
+            return;
+
+        Stack<Integer> curStack = null;
+        if (this.stackList.isEmpty() || (curStack = this.stackList.get(this.stackList.size() - 1)).size() >= this.cap) {
+            curStack = new Stack<>();
+            this.stackList.add(curStack);
+        }
+        curStack.push(val);
+    }
+
+    public int pop() {
+        return popAt(this.stackList.size() - 1);
+    }
+
+    public int popAt(int index) {
+        if (cap == 0 || index >= this.stackList.size() || index < 0) {
+            return -1;
+        }
+        Stack<Integer> curStack = null;
+        if ((curStack = this.stackList.get(index)).isEmpty()) {
+            return -1;
+        }
+        int res = curStack.pop();
+        if (curStack.isEmpty()) {
+            this.stackList.remove(index);
+        }
+        return res;
+    }
+}
