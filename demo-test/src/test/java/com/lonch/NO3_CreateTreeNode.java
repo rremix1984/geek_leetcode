6694a338be20f092509ca056f0d24a24a7fcf5d8
wangxiaozhe <rremix1984@gmail.com>
2024-04-15 00:08:01 +0800

    test

--- demo-test/src/test/java/com/lonch/NO3_CreateTreeNode.java
+++ demo-test/src/test/java/com/lonch/NO3_CreateTreeNode.java
@@ -3,8 +3,11 @@
  */
 package com.lonch;
 
+import com.lonch.util.Node;
 import com.lonch.util.TreeNode;
 import org.junit.Test;
+
+import static com.lonch.util.Node.printTree;
 import static java.lang.Math.pow;
 import static org.junit.Assert.assertEquals;
 
@@ -27,17 +30,15 @@ public class NO3_CreateTreeNode {
     @Test
     public void test() {
         int depth = 4;
+        Node node = null;
         assertEquals("[0, " +
                         "[1, [3, [7, [8], [4, [9, [10]], " +
                         "[2, [5, [11, [12], [6, [13, [14]]]",
-                cTree(depth, 0).toString());
-    }
+                node.toString());
+        // 2024/4/8  NO.1 没思路，能做出来
+        // 2024/4/9  NO.2 没思路，看答案做出来了，题不难
+        // 2024/4/14 NO.3 做出来了，有点瑕疵
 
-    public TreeNode<Integer> cTree(int depth, int index) {
-        // 2024/4/8 NO.1 没思路，能做出来
-        // 2024/4/9 NO.2 没思路，看答案做出来了，题不难
-        // TODO
-        return null;
     }
 
 }
