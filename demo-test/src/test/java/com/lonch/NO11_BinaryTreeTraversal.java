44782c82bf103fa1f8cae51221d4f2593660f298
wangxiaozhe1984 <wangxiaozhe1984@163.com>
2024-05-08 23:51:51 +0800

    test

--- demo-test/src/test/java/com/lonch/NO11_BinaryTreeTraversal.java
+++ demo-test/src/test/java/com/lonch/NO11_BinaryTreeTraversal.java
@@ -45,96 +45,53 @@ public class NO11_BinaryTreeTraversal {
         // 2024/4/11 NO.2 没做出来，思路对
         // 2024/4/12-13-14-15-16-18 NO.3-4-5-6-7-8 一遍过
         // 2024/4/30 NO.9 一遍过
-        // 2024/5/7  NO.10 一遍过
+        // 2024/5/7-8  NO.10-11 一遍过
         // 假设这里有三棵树的根节点，并且它们通过parent相互连接
-
-        // 复制 3 棵树
-        Node<Character> root2 = copy(root1);
-        Node<Character> root3 = copy(root2);
-
-        root1.parent = root3;
-        root3.parent = root2;
-        root2.parent = root1;
-
-        // 从任意节点开始遍历整棵树
-        String res = travel(root1.left.right.left);
-        out.println(res);
-        assertEquals("J E K B D H I A C F L M G N O" +
-                " A B D H I E J K C F L M G N O A B D H I E J K" +
-                " C F L M G N O ", res);
+        Node<Character> root2 = copys(root1, 26);
+        Node<Character> root3 = copys(root2, 26);
+        root1.parent = root2;
+        root2.parent = root3;
+        root3.parent = root1;
         printTree(root1);
+        val list = new ArrayList<Node<Character>>();
+        StringBuilder sb = new StringBuilder();
+        travels(root1.left.right, sb);
+        out.println(sb);
     }
 
-    public static <E> Node<E> copy(Node<E> root) {
-        if (root == null)
+    private void travels(Node<Character> cur, StringBuilder sb) {
+        if (cur == null || cur.data == '*')
+            return;
+
+        sb.append(cur.data + " ");
+
+        // 将当前节点的数据重置为特殊值来标记已访问
+        char val = cur.data;
+        cur.data = '*';
+
+        travels(cur.left, sb);
+        travels(cur.right, sb);
+        travels(cur.parent, sb);
+
+        // 在退出节点时，将其数据恢复为原始值
+        cur.data = val;
+    }
+
+    private Node<Character> copys(Node<Character> root1, int delta) {
+        if (root1 == null)
             return null;
 
-        Node<E> node = new Node<>(root.data);
+        Node<Character> node = new Node<>((char) (root1.data + delta));
 
-        // 设置父节点
-        node.left = copy(root.left);
+        node.left = copys(root1.left, delta);
         if (node.left != null)
             node.left.parent = node;
 
-        // 设置父节点
-        node.right = copy(root.right);
+        node.right = copys(root1.right, delta);
         if (node.right != null)
             node.right.parent = node;
 
         return node;
-    }
-
-    public String travel(Node<?> node) {
-        if (node == null)
-            return "";
-
-        StringBuilder sb = new StringBuilder();
-        dfs(node, sb);
-        return sb.toString();
-    }
-
-    private void dfs(Node node, StringBuilder sb) {
-        if (node == null || node.isVisit())
-            return;
-
-        sb.append(node.data + " ");
-        node.setVisit(true);
-
-        dfs(node.left, sb);
-        dfs(node.right, sb);
-        dfs(node.parent, sb);
-    }
-
-    private void levelOrderTraversal(Node root, StringBuilder sb) {
-        if (root == null)
-            return;
-
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(root);
-
-        HashSet<Node> visited = new HashSet<>();
-        visited.add(root);
-
-        while (!queue.isEmpty()) {
-            Node currentNode = queue.poll();
-            sb.append(currentNode.data + " ");
-
-            if (currentNode.left != null && !visited.contains(currentNode.left)) {
-                queue.add(currentNode.left);
-                visited.add(currentNode.left);
-            }
-
-            // 添加父节点的检查，避免无限循环
-            if (currentNode.parent != null && !visited.contains(currentNode.parent)) {
-                queue.add(currentNode.parent);
-                visited.add(currentNode.parent);
-            }
-
-            if (currentNode.right != null && !visited.contains(currentNode.right)) {
-                queue.add(currentNode.right);
-                visited.add(currentNode.right);
-            }
-        }
     }
 
 }
